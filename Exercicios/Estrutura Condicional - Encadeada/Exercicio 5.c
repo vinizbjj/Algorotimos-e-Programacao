62bd191d948c958519507e53f953171174af1ef6
vinizbjj <viniz.bjj@gmail.com>
2022-04-07 14:22:56 -0300

    main

--- Exercicios/Estrutura Condicional - Encadeada/Exercicio 5.c
+++ Exercicios/Estrutura Condicional - Encadeada/Exercicio 5.c
@@ -20,23 +20,23 @@ int main()
 
     if (n1 < n2 && n2 < n3)
     {
-        printf("\nnumeros %.2d %.2d %.2d \n", n1, n2, n3);
+        printf("\nnumeros %.2d %.2d %.2d \n\n", n1, n2, n3);
     }
     else if (n1 < n3 && n3 < n2)
     {
-        printf("\nnumeros %.2d %.2d %.2d \n", n1, n3, n2);
+        printf("\nnumeros %.2d %.2d %.2d \n\n", n1, n3, n2);
     }
     else if (n2 < n1 && n1 < n3)
     {
-        printf("\nnumeros %.2d %.2d %.2d \n", n2, n1, n3);
+        printf("\nnumeros %.2d %.2d %.2d \n\n", n2, n1, n3);
     }
     else if (n2 < n3 && n3 < n1)
     {
-        printf("\nnumeros %.2d %.2d %.2d \n", n2, n3, n1);
+        printf("\nnumeros %.2d %.2d %.2d \n\n", n2, n3, n1);
     }
     else if (n3 < n1 && n1 < n2)
     {
-        printf("\nnumeros %.2d %.2d %.2d \n", n3, n1, n2);
+        printf("\nnumeros %.2d %.2d %.2d \n\n", n3, n1, n2);
     }
     else if (n3 < n2 && n2 < n1)
     {
@@ -44,14 +44,14 @@ int main()
     }
     else if (n1 == n2 || n1 == n3 || n1 == n2 && n3 || n2 == n3)
     {
-        printf("\nNumeros digitados nao podem ser iguais\n");
+        printf("\nNumeros digitados nao podem ser iguais \n\n");
     }
     else
     {
-        printf("\nAlgo deu errado, reveja o codigo\n");
+        printf("\nAlgo deu errado, reveja o codigo \n\n");
     }
 
-    system("\n \nPause");
+    system("\n \nPause\n");
 
 }
     return 0;
